21a40f3d33ccb9e0388f4c671aefc6e951ec61d1
Latisha McCullum <m.latisha@yahoo.com>
2021-01-17 21:46:38 -0600

    added sorting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
 import Heading from "./components/Header";
-import SortTable from "./components/Table";
 import Search from "./components/Search";
 import TableRow from "./components/TableRow";
 import "./App.css";
 import { get } from "axios";
-
-const style = {
-  margin: "0",
-  color: "#ebb573",
-};
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
+      direction: "",
       searchField: "",
       people: [],
+      filteredEmployees: [],
     };
   }
 
@@ -26,52 +22,74 @@ class App extends Component {
       const {
         data: { results },
       } = await get("https://randomuser.me/api/?results=20&nat=us");
-      this.setState({ people: results });
+      this.setState({ people: results, filteredEmployees: results });
     };
     getPeople();
   }
 
   handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
+    const filteredEmployees = this.state.people.filter((employee) =>
+      employee.name.first.toLowerCase().includes(e.target.value.toLowerCase()) ||
+      employee.name.last.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+
+    this.setState({
+      searchField: e.target.value,
+      filteredEmployees: filteredEmployees,
+    });
   };
 
-  sortFirst = (e) => {
-    this.setState({ people: e });
+  sortFirst = () => {
+    this.setState({
+      filteredEmployees: this.state.filteredEmployees.sort(this.firstNameSort),
+    });
   };
 
-  sortLast = (e) => {
-    this.setState({ people: e });
+  firstNameSort = (a, b) => {
+    let sorting = 0;
+    if (a.name.first > b.name.first) {
+      if (this.state.direction === "DESC") {
+        sorting = 1;
+      } else {
+        sorting = -1;
+      }
+    } else if (a.name.first < b.name.first) {
+      if (this.state.direction === "DESC") {
+        sorting = -1;
+      } else {
+        sorting = 1;
+      }
+    }
+    let direction;
+    if (this.state.direction === "DESC") {
+      direction = "ASC";
+    } else {
+      direction = "DESC";
+    }
+    this.setState({ direction });
+    return sorting;
   };
 
   render() {
-    const { people, searchField } = this.state;
-    const filteredEmployees = people.filter((employee) =>
-      employee.name.first.toLowerCase().includes(searchField.toLowerCase())
-    );
-
     return (
       <>
-        <div style={style} className="App">
+        <div className="App">
           <Heading />
           <Search placeholder="" handleChange={this.handleChange} />
-          {/* <SortTable
-          people={filteredEmployees}
-          sortFirst={this.sortFirst}
-          sortLast={this.sortLast}
-        /> */}
-
           <table className="table table-striped table-hover">
             <thead>
               <tr>
-                <td>Name</td>
+                <td className="sortable" onClick={() => this.sortFirst()}>
+                  Name ({this.state.direction || "sortable"})
+                </td>
                 <td>Location</td>
                 <td>Picture</td>
                 <td>Email</td>
               </tr>
             </thead>
             <tbody>
-              {filteredEmployees.map((employee) => (
-                <TableRow employee={employee} />
+              {this.state.filteredEmployees.map((employee) => (
+                <TableRow key={employee.email} employee={employee} />
               ))}
             </tbody>
           </table>
